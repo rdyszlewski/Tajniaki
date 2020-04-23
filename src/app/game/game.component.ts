f836732e28480934211f01ed6a1c818a10d46e7f
Parabits <parabitsdev@gmail.com>
2020-04-23 21:03:01 +0200

    Fix updating cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -5,7 +5,6 @@ import { Role } from './role';
 import { PlayerService } from '../playerService';
 import { BossMessage } from './bossMessage';
 import { GameState } from './models/gameState';
-import { typeWithParameters } from '@angular/compiler/src/render3/util';
 import { Card, CardCreator } from './models/card';
 
 @Component({
@@ -19,41 +18,70 @@ export class GameComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    var that = this;
-    ConnectionService.subscribe("/user/queue/game/start", message=>{
-      var data = JSON.parse(message.body);
-      PlayerService.setRole(this.getRole(data["playerRole"]));
-      that.setGameState(data["gameState"]);
-      that.model.cards = that.createCards(data["cards"]);  
+    // var that = this;
+    this.subscribeStartEvent();
+    this.subscribeAnswerEvent();
+    this.subscriveClickEvent();
+    this.subscribeQuestionEvent();
+    this.subscribeEndEvent();
+    this.sendStartMessage();
+
+    this.preventRightClickMenu();
+  }
+
+  private preventRightClickMenu() {
+    document.addEventListener('contextmenu', event => event.preventDefault());
+  }
+
+  private sendStartMessage() {
+    ConnectionService.send("START", "/app/game/start");
+  }
+
+  private subscribeEndEvent() {
+    ConnectionService.subscribe("/user/queue/game/end", message => {
+      console.log(message);
     });
-    ConnectionService.subscribe("/user/queue/game/answer", message=>{
-      var data = JSON.parse(message.body);
-      var correct = data["correct"]; // TODO: można coś z tym zrobić
-      let card = that.getCard(data["word"]);
-      card.color = data["correctColor"];
-      card.checked = true;
-      that.setGameState(data["gameState"]);
+  }
+
+  private subscribeQuestionEvent() {
+    ConnectionService.subscribe("/user/queue/game/question", message => {
+      let data = JSON.parse(message.body);
+      this.setGameState(data["gameState"]);
     });
-    ConnectionService.subscribe("/user/queue/game/click", message=>{
+  }
+
+  private subscriveClickEvent() {
+    ConnectionService.subscribe("/user/queue/game/click", message => {
       console.log(message);
       var data = JSON.parse(message.body);
       let editedCards = data['editedCards'];
       let cards = this.createCards(editedCards);
       this.updateCards(cards);
-      // TODO: przetestować to
     });
-    ConnectionService.subscribe("/user/queue/game/question", message=>{
-      let data = JSON.parse(message.body);
-      that.setGameState(data["gameState"]);
-    });
-    ConnectionService.send("START", "/app/game/start");
+  }
 
-    document.addEventListener('contextmenu', event=>event.preventDefault());
+  private subscribeAnswerEvent() {
+    ConnectionService.subscribe("/user/queue/game/answer", message => {
+      var data = JSON.parse(message.body);
+      let clientCard = CardCreator.createCard(data['card']);
+      var correct = data["correct"]; // TODO: coś z tym zrobić
+      this.model.replaceCard(clientCard.word, clientCard);
+      this.setGameState(data["gameState"]);
+    });
+  }
+
+  private subscribeStartEvent() {
+    ConnectionService.subscribe("/user/queue/game/start", message => {
+      var data = JSON.parse(message.body);
+      PlayerService.setRole(this.getRole(data["playerRole"]));
+      PlayerService.setTeam(this.getTeam(data['playerTeam']));
+      this.setGameState(data["gameState"]);
+      this.model.cards = this.createCards(data["cards"]);
+    });
   }
 
   private updateCards(cards){
     cards.forEach(card => {
-      console.log(card);
       this.model.replaceCard(card.word, card);
     });
   }
@@ -63,18 +91,17 @@ export class GameComponent implements OnInit {
     cardsTextList.forEach(element => {
       cards.push(CardCreator.createCard(element));
     });
-    cards.sort((x1,x2)=>x1.id-x2.id);
+    cards = cards.sort((x1,x2)=>x1.id-x2.id);
     return cards
   }
 
-  private setGameState(dataJson){
-    console.log("Ustawianie stanu gry");
-    this.model.currentTeam = this.getTeam(dataJson["currentTeam"]);
-    this.model.currentStage = this.getRole(dataJson["currentStage"]);
-    this.model.remainingBlue = dataJson["remainingBlue"];
-    this.model.remainingRed = dataJson["remainingRed"];
-    this.model.currentWord = dataJson["currentWord"];
-    this.model.remainingAnswers = dataJson["remainingAnswers"];
+  private setGameState(data){
+    this.model.currentTeam = this.getTeam(data["currentTeam"]);
+    this.model.currentStage = this.getRole(data["currentStage"]);
+    this.model.remainingBlue = data["remainingBlue"];
+    this.model.remainingRed = data["remainingRed"];
+    this.model.currentWord = data["currentWord"];
+    this.model.remainingAnswers = data["remainingAnswers"];
   }
 
   // TODO: przenieść to do innej metody
@@ -92,7 +119,10 @@ export class GameComponent implements OnInit {
 
   // TODO: wymyślić fajniejszą nazwę
   sendBossMessage(){
-    let message =new BossMessage("Hasło", 3);
+    let wordInput = document.getElementById("wordInput") as HTMLInputElement;
+    let numberInput = document.getElementById("numberInput") as HTMLInputElement;
+    // TODO: dodać sprawdzenie poprawności
+    let message =new BossMessage(wordInput.value, Number(numberInput.value));
     ConnectionService.send(JSON.stringify(message),"/app/game/question");
   }
 
@@ -150,4 +180,26 @@ export class GameComponent implements OnInit {
     console.log(word);
     ConnectionService.send(word, '/app/game/flag');
   }
+
+  isBoss(){
+    return PlayerService.getRole()==Role.BOSS;
+  }
+
+  getClientTeam(){
+    // TODO: dodać jakieś informacje
+    console.log(PlayerService.getTeam());
+    return PlayerService.getTeam();
+  }
+
+  getClientRole(){
+    return PlayerService.getRole();
+  }
+
+  preventSpace()
+{
+  // TODO: napisć to trochę lepiej
+    var str=document.getElementById("wordInput") as HTMLInputElement;
+    var regex=/[^a-z]/gi;
+    str.value=str.value.replace(regex ,"");
+}
 }
